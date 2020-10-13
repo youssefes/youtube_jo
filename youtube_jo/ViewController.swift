3974319fe8773265413fb0349a14338eba32597f
youssefes <ssh://git@my.domain.cz:2222/project-name>
2020-10-13 15:24:24 +0200

    Initial Commit

---
+++ youtube_jo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube_jo
+//
+//  Created by youssef on 10/13/20.
+//  Copyright © 2020 youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
